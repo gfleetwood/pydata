398323eab954248bc514eba656c6efeaee9da6c4
gfleetwood <gfleetwood@protonmail.com>
2018-03-19 02:35:33 -0400

    Update

--- pydata/load_libs.py
+++ pydata/load_libs.py
@@ -2,7 +2,10 @@ import importlib
 
 def libs():
   
-  requirements = [['pd', 'pandas'], ['skl', 'sklearn'], ['np', 'numpy'], ['pn', 'plotnine'], ['plt', 'matplotlib.pyplot']]
+  requirements = [
+    ['pd', 'pandas'], ['skl', 'sklearn'], ['np', 'numpy'], ['pn', 'plotnine'], ['plt', 'matplotlib.pyplot'],
+    ['lz', 'logzero'], ['pandasql', 'pandasql'],
+    ]
   imported_libs = {lib[0]: importlib.import_module(lib[1]) for lib in requirements}
   print(
   ''' 
@@ -14,6 +17,10 @@ def libs():
   scikit-learn loaded as sk
   matplotlib.pyplot loaded as plt
   plotnine loaded as pn
+  
+  
+  pandasql is also loaded. For usage, please run `pdsql = lambda q: sqldf(q, globals())` and reference
+  `pdsql`.
   '''
   )
   
